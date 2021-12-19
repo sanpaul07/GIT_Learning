c1c36c38ddbabacac6951434817c98f00e13bd89
Sanjoy Paul <sanjoyp@yahoo.com>
2021-12-19 16:35:47 +0530

    minusNumber.java commit

---
+++ src/test/java/gitLearning/minusNumbers.java
@@ -0,0 +1,14 @@
+package gitLearning;
+
+public class minusNumbers {
+
+	public static void main(String[] args) {
+		
+		int a = 20;
+		int b = 10;
+		int c = a-b;
+		
+		System.out.println(c);
+	}
+
+}
